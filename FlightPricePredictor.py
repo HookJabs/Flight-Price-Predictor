6491fd5cdecc27a3ca1776030408199c56271c67
HookJabs <joshkong1@gmail.com>
2019-05-01 23:13:33 -0700

    Added data file and experimented with using pandas to display data

--- FlightPricePredictor.py
+++ FlightPricePredictor.py
@@ -1,2 +1,20 @@
 #Josh Kabo 2019
 #Flight Pricing Predictor
+
+from __future__ import print_function
+
+import pandas as pd
+pd.__version__
+
+airfare_report_dataframe = pd.read_csv("airfare_report.csv", sep=",")
+
+print("\n\ndf.describe:\n\n")
+print(airfare_report_dataframe.describe())
+
+print("\n\nSorted by fare\n\n")
+print(airfare_report_dataframe.sort_values('fare', ascending=True))
+
+print("\n\nSliced to just the origin and destination\n\n")
+print(airfare_report_dataframe[['origin', 'destination']])
+
+#TODO: Figure out the other columns like fare_lg, lf_ms, etc. Trim these columns out if necessary.
